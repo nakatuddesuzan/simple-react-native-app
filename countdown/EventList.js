a4e34b5bb30795d445d16a22bf7719673f56587b
Suzan Nakatudde <suzannakatudde@Suzans-MacBook-Pro.local>
2019-11-14 00:19:39 -0600

    create react-native app

---
+++ countdown/EventList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Text } from "react-native";
+
+class EventList extends Component {
+    state = {  }
+    render() { 
+        return ( <Text>Hello</Text> );
+    }
+}
+ 
+export default EventList;
